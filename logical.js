e43d94d74bcf35d35a5bc3fb9e906296e021a28c
Rayza Luana <93015921+RayzaAnchayhua@users.noreply.github.com>
2024-05-26 21:42:43 -0300

    commit-depois-de-clonar

--- logical.js
+++ logical.js
@@ -1,12 +1,12 @@
     function insert(num) 
     {
         var numero = document.getElementById('resultado').innerHTML;
-        document.getElementById('resultado').innerHTML = numero + num;
+        document.getElementById('resultado').innerHTML = (numero + num);
     }
 
     function clean()
     {
-        document.getElementById('resultado').innerHTML="";
+        document.getElementById('resultado').innerHTML=("");
     }
 
     function back()
