d65c14a627b9f87d5579250bb5c0ea244074703f
Rayza Luana <93015921+RayzaAnchayhua@users.noreply.github.com>
2024-06-10 20:53:13 -0300

    Arrumando a responsividade

--- logical.js
+++ logical.js
@@ -9,17 +9,35 @@ function clean() {
 
 function back() {
   var resultado = document.getElementById("resultado").innerHTML;
-  document.getElementById("resultado").innerHTML = resultado.substring(
-    0,
-    resultado.length - 1
-  );
+  document.getElementById("resultado").innerHTML = resultado.substring( 0, resultado.length - 1);
 }
 
-function calculate() {
+function result() {
   var resultado = document.getElementById("resultado").innerHTML;
   if (resultado) {
     document.getElementById("resultado").innerHTML = eval(resultado);
   } else {
-    document.getElementById("resultado").innerHTML = "Nada..";
+    document.getElementById("resultado").innerHTML = "";
   }
+}
+
+function brackets() {
+  var resultado = document.getElementById("resultado").innerHTML;{ 
+    if (
+      resultado.indexOf("(") == -1 ||
+      resultado.indexOf("(") != -1 &&  
+      resultado.indexOf(")") != -1 && 
+      resultado.lastIndexOf("(") < resultado.lastIndexOf(")")
+    ){
+      resultado += "(";
+    } else if (
+      resultado.indexOf("(") != -1 &&
+      resultado.indexOf(")") == -1 ||  
+      resultado.indexOf("(") != -1 && 
+      resultado.indexOf(")") != -1 && 
+      resultado.lastIndexOf("(") > resultado.lastIndexOf(")")
+  ){
+      resultado += ")";
+  }
+}
 }
